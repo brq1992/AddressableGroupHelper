159a57538733ad14c477be7e772c8b3ae031f61d
jeff <jeff.ban@igg.com>
2024-06-13 09:37:18 -0700

    refactor - add comment

--- Assets/AddressableGroupHelper/Editor/AddressabelUtilities.cs
+++ Assets/AddressableGroupHelper/Editor/AddressabelUtilities.cs
@@ -6,11 +6,18 @@ namespace AddressableAssetTool
 {
     internal class AddressabelUtilities
     {
+        /// <summary>
+        /// if true, will return built in resource dependencies.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="includeIndirect"></param>
+        /// <returns></returns>
         public static string[] GetDependPaths(string path, bool includeIndirect)
         {
             string[] dependPaths;
             if (includeIndirect)
             {
+                
                 var dependPathList = new List<string>();
                 Object asset = AssetDatabase.LoadAssetAtPath<Object>(path);
                 foreach (Object obj in EditorUtility.CollectDependencies(new Object[] { asset }))
