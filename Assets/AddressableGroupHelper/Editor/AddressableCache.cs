279357cbbd6ca60a526e2f505720b1c2bb885019
jeff <jeff.ban@igg.com>
2024-06-19 15:53:13 -0700

    feature - When trying to get the dependencies of a prefab, it should check if it is a variant of another prefab.                 If it is, it should also get all the dependencies of its base prefab.

--- Assets/AddressableGroupHelper/Editor/AddressableCache.cs
+++ Assets/AddressableGroupHelper/Editor/AddressableCache.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 
 namespace AddressableAssetTool
 {
@@ -28,7 +29,40 @@ namespace AddressableAssetTool
             {
                 return deps;
             }
-            deps = AssetDatabase.GetDependencies(assetPath, false);
+
+            // When trying to get the dependencies of a prefab, it should check if it is a variant of another prefab.
+            // If it is, it should also get all the dependencies of its base prefab.
+            List<string> list = new List<string>();
+            var assetObj = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+            var prefabType = PrefabUtility.GetPrefabAssetType(assetObj);
+            if (prefabType == PrefabAssetType.Variant)
+            {
+                var basePrefab = PrefabUtility.GetCorrespondingObjectFromSource(assetObj);
+                if (basePrefab != null)
+                {
+                    var basePrefabPath = AssetDatabase.GetAssetPath(basePrefab);
+                    var basePrefabDepenPaths = AssetDatabase.GetDependencies(basePrefabPath, false); //AddressabelUtilities.GetDependPaths(AssetDatabase.GUIDToAssetPath(guid), _includeIndirect);
+                    foreach (var basePrefabDepenPath in basePrefabDepenPaths)
+                    {
+                        if (!list.Contains(basePrefabDepenPath))
+                        {
+                            list.Add(basePrefabDepenPath);
+                        }
+                    }
+                }
+            }
+
+            var assetDependencyPaths = AssetDatabase.GetDependencies(assetPath, false);
+            for(int i = 0; i < assetDependencyPaths.Length; i++)
+            {
+                if (!list.Contains(assetDependencyPaths[i]))
+                {
+                    list.Add(assetDependencyPaths[i]);
+                }
+            }
+
+            deps = list.ToArray();
+
             noRecursiveDic.Add(assetPath, deps);
             return deps;
         }
@@ -40,7 +74,39 @@ namespace AddressableAssetTool
             {
                 return deps;
             }
-            deps = AssetDatabase.GetDependencies(assetPath, true);
+
+            // When trying to get the dependencies of a prefab, it should check if it is a variant of another prefab.
+            // If it is, it should also get all the dependencies of its base prefab.
+            List<string> list = new List<string>();
+            var assetObj = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+            var prefabType = PrefabUtility.GetPrefabAssetType(assetObj);
+            if (prefabType == PrefabAssetType.Variant)
+            {
+                var basePrefab = PrefabUtility.GetCorrespondingObjectFromSource(assetObj);
+                if (basePrefab != null)
+                {
+                    var basePrefabPath = AssetDatabase.GetAssetPath(basePrefab);
+                    var basePrefabDepenPaths = AssetDatabase.GetDependencies(basePrefabPath, true); //AddressabelUtilities.GetDependPaths(AssetDatabase.GUIDToAssetPath(guid), _includeIndirect);
+                    foreach (var basePrefabDepenPath in basePrefabDepenPaths)
+                    {
+                        if (!list.Contains(basePrefabDepenPath))
+                        {
+                            list.Add(basePrefabDepenPath);
+                        }
+                    }
+                }
+            }
+
+            var assetDependencyPaths = AssetDatabase.GetDependencies(assetPath, true);
+            for (int i = 0; i < assetDependencyPaths.Length; i++)
+            {
+                if (!list.Contains(assetDependencyPaths[i]))
+                {
+                    list.Add(assetDependencyPaths[i]);
+                }
+            }
+
+            deps = list.ToArray();
             recursiveDic.Add(assetPath, deps);
             return deps;
         }
